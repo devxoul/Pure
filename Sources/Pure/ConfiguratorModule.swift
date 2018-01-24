3469628e57ab3633dd59d8f0acd7eb78d5abc36f
Suyeol Jeon <devxoul@gmail.com>
2018-01-24 12:56:47 +0100

    Add shortcut initializers and methods to modules

--- Sources/Pure/ConfiguratorModule.swift
+++ Sources/Pure/ConfiguratorModule.swift
@@ -7,3 +7,21 @@ public protocol ConfiguratorModule: class, Module {
   /// Configures an existing module with a dependency and a payload.
   func configure(dependency: Dependency, payload: Payload)
 }
+
+public extension ConfiguratorModule where Dependency == Void {
+  public func configure(payload: Payload) {
+    self.configure(dependency: Void(), payload: payload)
+  }
+}
+
+public extension ConfiguratorModule where Payload == Void {
+  public func configure(dependency: Dependency) {
+    self.configure(dependency: dependency, payload: Void())
+  }
+}
+
+public extension ConfiguratorModule where Dependency == Void, Payload == Void {
+  public func configure() {
+    self.configure(dependency: Void(), payload: Void())
+  }
+}
--- Sources/Pure/FactoryModule.swift
+++ Sources/Pure/FactoryModule.swift
@@ -7,3 +7,21 @@ public protocol FactoryModule: Module {
   /// Creates an instance of a module with a dependency and a payload.
   init(dependency: Dependency, payload: Payload)
 }
+
+public extension FactoryModule where Dependency == Void {
+  public init(payload: Payload) {
+    self.init(dependency: Void(), payload: payload)
+  }
+}
+
+public extension FactoryModule where Payload == Void {
+  public init(dependency: Dependency) {
+    self.init(dependency: dependency, payload: Void())
+  }
+}
+
+public extension FactoryModule where Dependency == Void, Payload == Void {
+  public init() {
+    self.init(dependency: Void(), payload: Void())
+  }
+}
