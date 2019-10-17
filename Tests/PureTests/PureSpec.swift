b7a18c369ec8b475baf2a057e01d3ba7960d4ec2
Suyeol Jeon <devxoul@gmail.com>
2019-10-18 03:38:02 +0900

    Guarantee thread safety on Configurator

--- Tests/PureTests/PureSpec.swift
+++ Tests/PureTests/PureSpec.swift
@@ -132,6 +132,35 @@ final class PureSpec: QuickSpec {
 
         expect(dependencyInitializedCount) == 1
       }
+
+      it("is thread-safe") {
+        final class MyClass: FactoryModule {
+          static var dependencyInitializerExecutionCount = 0
+
+          struct Dependency {
+            init() {
+              MyClass.dependencyInitializerExecutionCount += 1
+              sleep(1)
+            }
+          }
+
+          struct Payload {
+          }
+
+          init(dependency: Dependency, payload: Payload) {
+          }
+        }
+
+        let factory = MyClass.Factory(dependency: .init())
+        for _ in 0..<100 {
+          DispatchQueue.global().async {
+            _ = factory.create(payload: .init())
+          }
+        }
+
+        XCTWaiter().wait(for: [XCTestExpectation()], timeout: 1)
+        expect(MyClass.dependencyInitializerExecutionCount) == 1
+      }
     }
 
     describe("a configurator") {
@@ -188,35 +217,35 @@ final class PureSpec: QuickSpec {
 
         expect(dependencyInitializedCount) == 1
       }
-    }
 
-    it("is thread-safe") {
-      final class MyClass: FactoryModule {
-        static var dependencyInitializerExecutionCount = 0
+      it("is thread-safe") {
+        final class MyClass: ConfiguratorModule {
+          static var dependencyInitializerExecutionCount = 0
 
-        struct Dependency {
-          init() {
-            MyClass.dependencyInitializerExecutionCount += 1
-            sleep(1)
+          struct Dependency {
+            init() {
+              MyClass.dependencyInitializerExecutionCount += 1
+              sleep(1)
+            }
+          }
+
+          struct Payload {
+          }
+
+          func configure(dependency: Dependency, payload: Payload) {
           }
         }
 
-        struct Payload {
+        let configurator = MyClass.Configurator(dependency: .init())
+        for _ in 0..<100 {
+          DispatchQueue.global().async {
+            _ = configurator.configure(MyClass(), payload: .init())
+          }
         }
 
-        init(dependency: Dependency, payload: Payload) {
-        }
+        XCTWaiter().wait(for: [XCTestExpectation()], timeout: 1)
+        expect(MyClass.dependencyInitializerExecutionCount) == 1
       }
-
-      let factory = MyClass.Factory(dependency: .init())
-      for _ in 0..<100 {
-        DispatchQueue.global().async {
-          _ = factory.create(payload: .init())
-        }
-      }
-
-      XCTWaiter().wait(for: [XCTestExpectation()], timeout: 1)
-      expect(MyClass.dependencyInitializerExecutionCount) == 1
     }
   }
 }
