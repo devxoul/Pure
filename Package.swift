fe8334d45d987cf468a3b594e691b37755f1df96
Suyeol Jeon <devxoul@gmail.com>
2020-06-14 04:41:00 +0900

    Update Quick and Nimble SPM dependency

--- Package.swift
+++ Package.swift
@@ -9,8 +9,8 @@ let package = Package(
   ],
   products: [.library(name: "Pure", targets: ["Pure"])],
   dependencies: [
-    .package(url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "2.0.0")),
-    .package(url: "https://github.com/Quick/Nimble.git", .revision("fc3cd1adf6961a3541679342d64fbbfd7a9f4ca7")),
+    .package(url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "3.0.0")),
+    .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "8.0.0")),
   ],
   targets: [
     .target(name: "Pure", dependencies: []),
